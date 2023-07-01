ddf42a96ff065e63b0b7c5d7088b8303310a5af0
Rachel <rachelbrockman1@gmail.com>
2023-07-01 06:18:09 -0400

    practice

--- boot.py
+++ boot.py
@@ -738,4 +738,54 @@
 #     [1, 2, 2, 3, 4, 5, 6, 7, 8, 100, 101, 103],
 # )
 
+
 # # ################################################################################
+class Archer:
+    def __init__(self, num_arrows, health, name):
+        self.num_arrows = num_arrows
+        self.health = health
+        self.name = name
+
+    def get_shot(self):
+        if self.health >= 1:
+            self.health -= 1
+        else:
+            print(f"{self.name} is dead")
+
+    def shoot(self, target):
+        if self.num_arrows <= 0:
+            raise Exception(f"{self.name} can't shoot")
+        else:
+            self.num_arrows -= 1
+            print(f"{self.name} {target.name}")
+            target.get_shot()
+
+
+# don't touch below this line
+
+
+def main():
+    bard = Archer(1, 3, "Bard")
+    legolas = Archer(10000, 3, "Legolas")
+
+    while bard.health > 0 and legolas.health > 0:
+        try:
+            print_status(bard)
+            print_status(legolas)
+            bard.shoot(legolas)
+        except Exception as e:
+            print(e)
+
+        try:
+            print_status(bard)
+            print_status(legolas)
+            legolas.shoot(bard)
+        except Exception as e:
+            print(e)
+
+
+def print_status(archer):
+    print(f"{archer.name} has {archer.health} health and {archer.num_arrows} arrows")
+
+
+main()
