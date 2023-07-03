d06034099e9a63b4b0d799b99b98afbec7f51574
Rachel <rachelbrockman1@gmail.com>
2023-07-03 15:31:51 -0400

    deck of cards OOP practice

--- boot.py
+++ boot.py
@@ -926,84 +926,153 @@
 
 # main()
 # # # ################################################################################
-# CALCULATOR
-class Calculator:
+# # CALCULATOR
+# class Calculator:
+#     def __init__(self):
+#         self.__result = 0
+
+#     def add(self, a):
+#         self.__result += a
+
+#     def subtract(self, a):
+#         self.__result -= a
+
+#     def multiply(self, a):
+#         self.__result *= a
+
+#     def divide(self, a):
+#         if a == 0:
+#             raise ValueError("Cannot divide by zero")
+#         else:
+#             self.__result /= a
+
+#     def modulo(self, a):
+#         if a == 0:
+#             raise ValueError("Cannot divide by zero")
+#         else:
+#             self.__result %= a
+
+#     def power(self, a):
+#         self.__result **= a
+
+#     def square_root(self):
+#         self.__result **= 0.5
+
+#     def clear(self):
+#         self.__result = 0
+
+#     def get_result(self):
+#         return self.__result
+
+
+# # don't touch below this line
+
+
+# def test(starting_num):
+#     calculator = Calculator()
+#     calculator.add(starting_num)
+#     print(f"Starting number: {starting_num}")
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Adding 5...")
+#     calculator.add(5)
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Modulo 7...")
+#     calculator.modulo(7)
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Power 2...")
+#     calculator.power(2)
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Square root...")
+#     calculator.square_root()
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Subtracting 3...")
+#     calculator.subtract(3)
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Multiplying by 2...")
+#     calculator.multiply(2)
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Dividing by 6...")
+#     calculator.divide(6)
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print(f"Clearing...")
+#     calculator.clear()
+#     print(f"Result: {calculator.get_result():.2f}")
+#     print("=====================================")
+
+
+# def main():
+#     test(11.0)
+#     test(6.0)
+#     test(0.0)
+
+
+# main()
+# # ################################################################################
+import random
+
+
+class DeckOfCards:
+    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    RANKS = [
+        "Ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "Jack",
+        "Queen",
+        "King",
+    ]
+
     def __init__(self):
-        self.__result = 0
+        self.__cards = []
+        self.create_deck()
 
-    def add(self, a):
-        self.__result += a
+    def create_deck(self):
+        for suit in self.SUITS:
+            for rank in self.RANKS:
+                card = (rank, suit)
+                self.__cards.append(card)
 
-    def subtract(self, a):
-        self.__result -= a
+    def shuffle_deck(self):
+        random.shuffle(self.__cards)
 
-    def multiply(self, a):
-        self.__result *= a
+    def deal_card(self):
+        if len(self.__cards) == 0:
+            return None
+        return self.__cards.pop(0)
 
-    def divide(self, a):
-        if a == 0:
-            raise ValueError("Cannot divide by zero")
-        else:
-            self.__result /= a
-
-    def modulo(self, a):
-        if a == 0:
-            raise ValueError("Cannot divide by zero")
-        else:
-            self.__result %= a
-
-    def power(self, a):
-        self.__result **= a
-
-    def square_root(self):
-        self.__result **= 0.5
-
-    def clear(self):
-        self.__result = 0
-
-    def get_result(self):
-        return self.__result
+    # don't touch below this line
+    def __str__(self):
+        return f"The deck has {len(self.__cards)} cards"
 
 
-# don't touch below this line
-
-
-def test(starting_num):
-    calculator = Calculator()
-    calculator.add(starting_num)
-    print(f"Starting number: {starting_num}")
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Adding 5...")
-    calculator.add(5)
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Modulo 7...")
-    calculator.modulo(7)
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Power 2...")
-    calculator.power(2)
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Square root...")
-    calculator.square_root()
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Subtracting 3...")
-    calculator.subtract(3)
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Multiplying by 2...")
-    calculator.multiply(2)
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Dividing by 6...")
-    calculator.divide(6)
-    print(f"Result: {calculator.get_result():.2f}")
-    print(f"Clearing...")
-    calculator.clear()
-    print(f"Result: {calculator.get_result():.2f}")
+def test(deck):
+    print(deck)
+    print("Dealing a hand:")
+    hand = []
+    for i in range(5):
+        card = deck.deal_card()
+        if card is None:
+            print("Out of cards!")
+            break
+        print(f" - {card[0]} of {card[1]}")
     print("=====================================")
+    return deck
 
 
 def main():
-    test(11.0)
-    test(6.0)
-    test(0.0)
+    random.seed(1)
+    deck = DeckOfCards()
+    deck.shuffle_deck()
+    deck = test(deck)
+    deck = test(deck)
+    deck = test(deck)
 
 
 main()
