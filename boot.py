3bf2c6b2630bfe5923b7b8f793ce13a8a74d1bf1
Rachel <rachelbrockman1@gmail.com>
2023-07-08 06:37:15 -0400

    influence score

--- boot.py
+++ boot.py
@@ -1195,66 +1195,95 @@
 
 # main()
 # # ################################################################################
-def main():
-    dragons = [
-        Dragon("Green Dragon", 0, 0, 1),
-        Dragon("Red Dragon", 2, 2, 2),
-        Dragon("Blue Dragon", 4, 3, 3),
-        Dragon("Black Dragon", 5, -1, 4),
-    ]
+# def main():
+#     dragons = [
+#         Dragon("Green Dragon", 0, 0, 1),
+#         Dragon("Red Dragon", 2, 2, 2),
+#         Dragon("Blue Dragon", 4, 3, 3),
+#         Dragon("Black Dragon", 5, -1, 4),
+#     ]
 
-    # don't touch above this line
+#     # don't touch above this line
 
-    for dragon in dragons:
-        describe(dragon)
-    dragons_copy = dragons.copy()
+#     for dragon in dragons:
+#         describe(dragon)
+#     dragons_copy = dragons.copy()
 
-    for dragon in dragons:
-        dragons_copy.remove(dragon)
-        dragon.breathe_fire(3, 3, dragons_copy)
-        dragons_copy.append(dragon)
+#     for dragon in dragons:
+#         dragons_copy.remove(dragon)
+#         dragon.breathe_fire(3, 3, dragons_copy)
+#         dragons_copy.append(dragon)
+
+
+# # don't touch below this line
+
+
+# def describe(dragon):
+#     print(f"{dragon.name} is at {dragon.pos_x}/{dragon.pos_y}")
+
+
+# class Unit:
+#     def __init__(self, name, pos_x, pos_y):
+#         self.name = name
+#         self.pos_x = pos_x
+#         self.pos_y = pos_y
+
+#     def in_area(self, x_1, y_1, x_2, y_2):
+#         return (
+#             self.pos_x >= x_1
+#             and self.pos_x <= x_2
+#             and self.pos_y >= y_1
+#             and self.pos_y <= y_2
+#         )
+
+
+# class Dragon(Unit):
+#     def __init__(self, name, pos_x, pos_y, fire_range):
+#         super().__init__(name, pos_x, pos_y)
+#         self.__fire_range = fire_range
+
+#     def breathe_fire(self, x, y, units):
+#         print(f"{self.name} breathes fire at {x}/{y} with range {self.__fire_range}")
+#         for unit in units:
+#             in_area = unit.in_area(
+#                 x - self.__fire_range,
+#                 y - self.__fire_range,
+#                 x + self.__fire_range,
+#                 y + self.__fire_range,
+#             )
+#             if in_area:
+#                 print(f"{unit.name} is hit by the fire")
+
+
+# main()
+
+# # ################################################################################
+import math
+
+
+def get_influencer_score(num_followers, average_engagement_percentage):
+    influencer_score = average_engagement_percentage * math.log(num_followers, 2)
+    return influencer_score
 
 
 # don't touch below this line
 
 
-def describe(dragon):
-    print(f"{dragon.name} is at {dragon.pos_x}/{dragon.pos_y}")
+def test(num_followers, average_engagement_percentage):
+    influencer_score = round(
+        get_influencer_score(num_followers, average_engagement_percentage)
+    )
+    print(f"- num_followers: {num_followers}")
+    print(f"- average_engagement_percentage: {average_engagement_percentage}")
+    print(f"Influencer score: {influencer_score}")
+    print("====================================")
 
 
-class Unit:
-    def __init__(self, name, pos_x, pos_y):
-        self.name = name
-        self.pos_x = pos_x
-        self.pos_y = pos_y
-
-    def in_area(self, x_1, y_1, x_2, y_2):
-        return (
-            self.pos_x >= x_1
-            and self.pos_x <= x_2
-            and self.pos_y >= y_1
-            and self.pos_y <= y_2
-        )
-
-
-class Dragon(Unit):
-    def __init__(self, name, pos_x, pos_y, fire_range):
-        super().__init__(name, pos_x, pos_y)
-        self.__fire_range = fire_range
-
-    def breathe_fire(self, x, y, units):
-        print(f"{self.name} breathes fire at {x}/{y} with range {self.__fire_range}")
-        for unit in units:
-            in_area = unit.in_area(
-                x - self.__fire_range,
-                y - self.__fire_range,
-                x + self.__fire_range,
-                y + self.__fire_range,
-            )
-            if in_area:
-                print(f"{unit.name} is hit by the fire")
+def main():
+    test(40000, 0.3)
+    test(43000, 0.1)
+    test(100000, 0.6)
+    test(200, 0.8)
 
 
 main()
-
-# # ################################################################################
