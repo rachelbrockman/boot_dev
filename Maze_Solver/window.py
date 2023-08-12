c42554b277e26b98512b2bb34bf5ef25eebdfde6
Rachel <rachelbrockman1@gmail.com>
2023-08-11 20:46:35 -0400

    finish window class

--- Maze_Solver/window.py
+++ Maze_Solver/window.py
@@ -1,9 +1,13 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, width, height, title):
         self.width = width
         self.height = height
-
-def new_root(self):
-
+        self.root = Tk()  # create new root widget
+        self.root.title(title)  # set the title property of the root widget
+        self.canvas = Canvas(self.root, width=self.width, height=self.height)  # create
+        # a Canvas and save it as a data member
+        self.canvas.pack()
+        self.running = False
